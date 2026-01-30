b7d6b6eb4ae8f1bc7f66391cbffaf0b446bc26b5
Rafiul-Hossain <rafiulhossain4393@gmail.com>
2026-01-30 15:00:26 +0600

    Enhance career and case study models and services; add new fields for role, description, requirements, responsibilities, and case study details

--- src/entities/career/career.model.js
+++ src/entities/career/career.model.js
@@ -5,9 +5,13 @@ export const CAREER_TYPE_OPTIONS = ['full time', 'part-time', 'freelance'];
 const CareerSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true },
+    role: { type: String, required: true, trim: true },
     department: { type: String, required: true, trim: true },
     location: { type: String, required: true, trim: true },
     type: { type: String, enum: CAREER_TYPE_OPTIONS, required: true, trim: true },
+    description: { type: String, trim: true },
+    requirements: { type: String, trim: true },
+    responsibilities: { type: String, trim: true },
   },
   { timestamps: true }
 );
